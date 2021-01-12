044bec190398deb730abf9efcad6677fcc543315
samryan121 <77112103+samryan121@users.noreply.github.com>
2021-01-12 14:10:30 +0000

    Update flanker_main.js

--- flanker_main.js
+++ flanker_main.js
@@ -1,4 +1,8 @@
- /* experiment parameters */
+     // Use JSDELIVR to get the files from a GitHub repository
+// https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<specific folder or file>
+var repo_site = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/";
+
+        /* experiment parameters */
         var reps_per_trial_type = 4;
 
         /*set up welcome block*/
@@ -85,6 +89,4 @@
         timeline.push(test);
         timeline.push(debrief);
         
-        // Use JSDELIVR to get the files from a GitHub repository
-// https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/<specific folder or file>
-var repo_site = "https://cdn.jsdelivr.net/gh/kywch/jsPsych-in-Qualtrics/flanker/";
+    
